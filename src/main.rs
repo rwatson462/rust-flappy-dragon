96a1bd662dd5e1e6ed64a35c53f155ff283044f8
Rob Watson <9914552+rwatson462@users.noreply.github.com>
2022-04-16 21:06:34 +0100

    Add obstacles

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bracket_lib::prelude::*;
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const FRAME_DURATION: f32 = 75.0;
+const WINDOW_BG: (u8,u8,u8) = NAVY;
 
 enum GameMode {
     Menu,
@@ -26,11 +27,30 @@ impl Player {
     }
 }
 
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10,40),
+            size: i32::max(2, 20-score)
+        }
+    }
+}
+
 struct State {
     mode: GameMode,
     player: Player,
     frame_time: f32,
-    paused: bool
+    paused: bool,
+    score: i32,
+    obstacles: Vec<Obstacle>
 }
 
 impl State {
@@ -39,12 +59,14 @@ impl State {
             mode: GameMode::Menu,
             player: Player::new(5,20),
             frame_time: 0.0,
-            paused: false
+            paused: false,
+            score: 0,
+            obstacles: vec!()
         }
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
+        ctx.cls_bg(WINDOW_BG);
         ctx.print(1,1, "Hello, Flappy Dragon!");
         // todo: wait for user input before starting a game
         ctx.print(2,5, "(P) Play");
@@ -76,13 +98,17 @@ impl State {
             }
         }
 
-        ctx.cls_bg(NAVY);
-        ctx.print(1,1, "Press [SPACE] to fly!");
+        ctx.cls_bg(WINDOW_BG);
+        self.render_obstacles(ctx);
         self.render_player(ctx);
+        ctx.print(1,1, "Press [SPACE] to fly!");
     }
 
     fn update(&mut self) {
         self.frame_time = 0.0;
+
+        // add a new obstable if required
+        self.add_obstacle();
             
         // apply gravity to dragon
         self.gravity_and_move();
@@ -91,22 +117,48 @@ impl State {
         // we use player.x to simulate horizontal movement
         self.player.x += 1;
 
-
-        let mut colliding = false;
-        // check for collisions with walls
-        // check for collision with ground
-        if self.player.y > SCREEN_HEIGHT {
-            colliding = true;
-        }
-
-        if colliding == true {
+        if self.is_collision() {
             self.mode = GameMode::End;
         }
     }
+
+    fn is_collision(&mut self) -> bool {
+        // check for collisions with walls
+        for obstacle in &self.obstacles {
+            if self.player.x != obstacle.x {
+                continue;
+            }
+            
+            let half_size = obstacle.size / 2;
+            if self.player.y < obstacle.gap_y - half_size {
+                return true;
+            }
+
+            if self.player.y > obstacle.gap_y + half_size {
+                return true;
+            }
+        }
+
+        // check for collision with ground
+        if self.player.y > SCREEN_HEIGHT {
+            return true;
+        }
+
+        false
+    }
     
     fn game_over(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
+        ctx.cls_bg(WINDOW_BG);
         ctx.print(10,10, "Game over, man");
+        ctx.print(10,12, "Press [SPACE] to return to the menu");
+
+        // handle key presses
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::Space => self.mode = GameMode::Menu,
+                _ => {}
+            }
+        }
     }
 
     fn restart(&mut self) {
@@ -115,12 +167,47 @@ impl State {
         self.mode = GameMode::Playing;
     }
 
+    fn add_obstacle(&mut self) {
+        if self.obstacles.len() < 2 {
+            self.obstacles.push(Obstacle::new(SCREEN_WIDTH+self.player.x, 10))
+        }
+    }
+
+    fn render_obstacles(&mut self, ctx: &mut BTerm) {
+        let player_x = self.player.x;
+        for obstacle in &self.obstacles {
+            let screen_x = obstacle.x - player_x;
+            let half_size = obstacle.size / 2;
+            
+            for y in 0..obstacle.gap_y - half_size {
+                ctx.set(
+                    screen_x,
+                    y,
+                    RED,
+                    WINDOW_BG,
+                    to_cp437('|')
+                );
+            }
+
+            for y in obstacle.gap_y + half_size..SCREEN_HEIGHT {
+                ctx.set(
+                    screen_x,
+                    y,
+                    RED,
+                    WINDOW_BG,
+                    to_cp437('|')
+                );
+            }
+
+        }
+    }
+
     fn render_player(&mut self, ctx: &mut BTerm) {
         ctx.set(
             5,
             self.player.y,
             YELLOW,
-            BLACK,
+            WINDOW_BG,
             to_cp437('@')
         );
     }
