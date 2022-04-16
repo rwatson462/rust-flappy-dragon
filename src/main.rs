0aa7aaf5031fdef62d362f5ad3a44e8be9e9e917
Rob Watson <9914552+rwatson462@users.noreply.github.com>
2022-04-16 19:23:36 +0100

    Add game states

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,45 @@
 use bracket_lib::prelude::*;
 
-struct State {
+enum GameMode {
+    Menu,
+    Playing,
+    End
+}
 
+struct State {
+    mode: GameMode
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1,1, "Hello, Flappy Dragon!");
+    }
+
+    fn playing(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(10,10, "Playing the game");
+    }
+    
+    fn game_over(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(40,10, "Game over, man");
+    }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1,1, "Hello, Flappy Dragon!");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.playing(ctx),
+            GameMode::End => self.game_over(ctx)
+        };
     }
 }
 
@@ -15,5 +47,5 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
-    main_loop(context,State{})
+    main_loop(context,State::new())
 }
